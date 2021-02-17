d9ea1677659a206dd12d783ab83be2d46919aca0
mcflugen <mcflugen@gmail.com>
2021-02-16 22:31:00 -0700

    remove azimuth to math angle transforms

--- tests/test_units.py
+++ tests/test_units.py
@@ -11,7 +11,6 @@ from gimli import (
     UnitStatus,
     UnitSystem,
 )
-from gimli.units import transform_azimuth_to_math, transform_math_to_azimuth
 
 
 @pytest.fixture
@@ -220,27 +219,3 @@ def test_unit_converter_inverse(system):
     seconds_to_hours = system.Unit("s").to(system.Unit("h"))
     hours_to_seconds = system.Unit("h").to(system.Unit("s"))
     assert hours_to_seconds(seconds_to_hours(val)) == pytest.approx(val)
-
-
-def test_math_to_azimuth():
-    angle = transform_math_to_azimuth(np.pi * 0.5, "rad")
-    assert angle == pytest.approx(0.0)
-
-
-def test_math_to_azimuth_inplace():
-    x = np.array([0.0, 0.5, 1.0, 1.5]) * np.pi
-    angle = transform_math_to_azimuth(x, "rad")
-    assert angle is x
-    assert angle == pytest.approx([np.pi * 0.5, 0.0, -np.pi * 0.5, -np.pi])
-
-
-def test_azimuth_to_math():
-    angle = transform_azimuth_to_math(0.0, "rad")
-    assert angle == pytest.approx(np.pi * 0.5)
-
-
-def test_azimuth_to_math_inplace():
-    x = np.array([np.pi * 0.5, 0.0, -np.pi * 0.5, -np.pi])
-    angle = transform_azimuth_to_math(x, "rad")
-    assert angle is x
-    assert angle == pytest.approx([0.0, np.pi * 0.5, np.pi, np.pi * 1.5])
