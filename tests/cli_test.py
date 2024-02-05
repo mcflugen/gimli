346c2139023a70e5e8822cb3a5e3e889b1a44cbc
mcflugen <mcflugen@gmail.com>
2024-02-05 16:49:54 -0400

    add more cli tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,80 +1,81 @@
+import contextlib
 from io import StringIO
 
 import numpy as np
 import pytest
-from click.testing import CliRunner
 from numpy.testing import assert_array_almost_equal
 
-from gimli import cli
+from gimli.main import main
 
 
-def test_command_line_interface():
+def test_cli_version(capsys):
+    with contextlib.suppress(SystemExit):
+        assert main(["--version"]) == 0
+    output = capsys.readouterr().out
+
+    assert "gimli" in output
+
+
+def test_command_line_interface(capsys):
     """Test the CLI."""
-    runner = CliRunner(mix_stderr=False)
-    result = runner.invoke(cli.gimli, ["--help"])
-    assert result.exit_code == 0
-    assert result.stdout.startswith("Usage: gimli")
-
-    result = runner.invoke(cli.gimli, ["--version"])
-    assert result.exit_code == 0
-    assert "version" in result.stdout
+    with contextlib.suppress(SystemExit):
+        assert main(["--help"]) == 0
+    assert capsys.readouterr().out.startswith("usage: gimli")
 
 
-def test_no_op():
-    runner = CliRunner(mix_stderr=False)
-    result = runner.invoke(cli.gimli)
-    assert result.exit_code == 0
-    assert result.stdout == ""
+def test_no_op(capsys):
+    with contextlib.suppress(SystemExit):
+        assert main(["--from=m", "--to=m"]) == 0
+    assert capsys.readouterr().out == ""
 
 
 @pytest.mark.parametrize("shape", [(-1,), (-1, 1), (3, -1)])
-def test_from_file(tmpdir, shape):
+def test_from_file(tmpdir, shape, capsys):
     values = np.arange(12.0).reshape(shape)
     with tmpdir.as_cwd():
-        runner = CliRunner(mix_stderr=False)
-
         np.savetxt("values.txt", values, delimiter=",")
 
-        result = runner.invoke(cli.gimli, ["values.txt"])
-        data = np.loadtxt(StringIO(result.stdout), delimiter=",")
+        with contextlib.suppress(SystemExit):
+            assert (
+                main(
+                    [
+                        *("-f", "m"),
+                        *("-t", "m"),
+                        "values.txt",
+                    ]
+                )
+                == 0
+            )
 
-        assert result.exit_code == 0
+        data = np.loadtxt(StringIO(capsys.readouterr().out), delimiter=",")
         assert_array_almost_equal(np.squeeze(values), np.squeeze(data))
 
 
 @pytest.mark.parametrize(
     "values", ["1,2,3,4,5", "2", "3.0", "1e-3", "0.1, 1e2", "-2.3"]
 )
-def test_opt_data(tmpdir, values):
+def test_opt_to_from(tmpdir, values, capsys):
     with tmpdir.as_cwd():
-        runner = CliRunner(mix_stderr=False)
+        with open("values.txt", "w") as fp:
+            print(values, file=fp)
 
-        result = runner.invoke(cli.gimli, [f"--data={values}"])
-        data = np.loadtxt(StringIO(result.stdout), delimiter=",")
-        expected = np.loadtxt(StringIO(values), delimiter=",")
+        with contextlib.suppress(SystemExit):
+            assert (
+                main(
+                    [
+                        *("-f", "m"),
+                        *("-t", "km"),
+                        "values.txt",
+                    ]
+                )
+                == 0
+            )
+        actual = np.loadtxt(StringIO(capsys.readouterr().out), delimiter=",")
+        expected = np.loadtxt(StringIO(values), delimiter=",") / 1000.0
 
-        assert result.exit_code == 0, result.stderr
-        assert_array_almost_equal(data, expected)
-
-
-def test_opt_to_from(tmpdir):
-    values = "1,2,3,4,5"
-    with tmpdir.as_cwd():
-        runner = CliRunner(mix_stderr=False)
-
-        result = runner.invoke(cli.gimli, ["--from=m", "--to=km", f"--data={values}"])
-        data = np.loadtxt(StringIO(result.stdout), delimiter=",")
-
-        assert result.exit_code == 0, result.stderr
-        assert_array_almost_equal(data * 1000.0, [1, 2, 3, 4, 5])
+        assert_array_almost_equal(actual, expected)
 
 
-def test_opt_to_from_incomaptible(tmpdir):
-    values = "1,2,3,4,5"
-    with tmpdir.as_cwd():
-        runner = CliRunner(mix_stderr=False)
-
-        result = runner.invoke(cli.gimli, ["--from=m", "--to=kg", f"--data={values}"])
-
-        assert result.exit_code != 0
-        assert result.stderr.startswith("incompatible units")
+def test_opt_to_from_incomaptible(tmpdir, capsys):
+    with tmpdir.as_cwd(), contextlib.suppress(SystemExit):
+        assert main(["--from=m", "--to=kg"]) != 0
