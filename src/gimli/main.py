5f1d346e46f9682461b6e09ec7611e2ace84458e
mcflugen <mcflugen@gmail.com>
2024-02-06 17:20:47 -0500

    better logging

--- src/gimli/main.py
+++ src/gimli/main.py
@@ -14,6 +14,8 @@ from gimli.utils import err
 from gimli.utils import out
 
 system = UnitSystem()
+load_values = partial(np.loadtxt, delimiter=",")
+dump_values = partial(np.savetxt, sys.stdout, delimiter=", ", fmt="%g")
 
 
 def main(argv: tuple[str, ...] | None = None) -> int:
@@ -28,39 +30,49 @@ def main(argv: tuple[str, ...] | None = None) -> int:
         ),
     )
     parser.add_argument(
-        "-v",
-        "--verbose",
+        *("-v", "--verbose"),
         action="store_true",
         help="Also emit status messages to stderr.",
     )
     parser.add_argument("file", type=argparse.FileType("rb"), nargs="*")
-    parser.add_argument("-t", "--to", action=Units, metavar="UNIT", default="1")
     parser.add_argument(
-        "-f", "--from", dest="from_", metavar="UNIT", action=Units, default="1"
+        *("-f", "--from"),
+        dest="from_",
+        metavar="UNIT",
+        action=UnitType,
+        default=system.Unit("1"),
+        help="Source units.",
+    )
+    parser.add_argument(
+        *("-t", "--to"),
+        action=UnitType,
+        metavar="UNIT",
+        default=system.Unit("1"),
+        help="Destination units.",
     )
 
     args = parser.parse_args(argv)
 
-    load = partial(np.loadtxt, delimiter=",")
-    dump = partial(np.savetxt, sys.stdout, delimiter=", ", fmt="%f")
-
     try:
         src_to_dst = args.from_.to(args.to)
     except IncompatibleUnitsError:
-        err(f"incompatible units: {args.from_!s}, {args.to!s}")
+        err(f"[error] incompatible units: {args.from_!s}, {args.to!s}")
         return 1
 
-    out(f"Convering {args.from_!s} -> {args.to!s}")
-    out(f"1.0 -> {src_to_dst(1.0)}")
+    if not args.quiet:
+        out(f"[info] Convering {args.from_!s} -> {args.to!s}")
+        out(f"[info] 1.0 -> {src_to_dst(1.0)}")
 
     for name in args.file:
-        array = load(name)
-        dump(np.atleast_1d(src_to_dst(array, out=array)))
+        if args.verbose and not args.quiet:
+            out(f"[info] reading {name.name}")
+        array = load_values(name)
+        dump_values(np.atleast_1d(src_to_dst(array, out=array)))
 
     return 0
 
 
-class Units(argparse.Action):
+class UnitType(argparse.Action):
     def __call__(
         self,
         parser: argparse.ArgumentParser,
@@ -69,11 +81,11 @@ class Units(argparse.Action):
         option_string: str | None = None,
     ) -> None:
         if not isinstance(values, str):
-            parser.error(f"{values!r}: invalid unit string: not a string")
+            parser.error(f"[error] {values!r}: invalid unit string: not a string")
 
         try:
             units = system.Unit(values)
         except UnitNameError:
-            parser.error(f"unknown or poorly-formed unit: {values!r}")
+            parser.error(f"[error] unknown or poorly-formed unit: {values!r}")
         else:
             setattr(namespace, self.dest, units)
