9a6ca4f3f88b2183074552188cdf8c0959102ab8
mcflugen <mcflugen@gmail.com>
2024-02-07 10:19:07 -0700

    vendor udunits-2

---
+++ src/gimli/data/udunits-2/config.h.cmake
@@ -0,0 +1,8 @@
+/*
+ * C macros set by cmake(1)
+ */
+#define DEFAULT_UDUNITS2_XML_PATH "@DEFAULT_UDUNITS2_XML_PATH@"
+#cmakedefine DLL_UDUNITS2
+#cmakedefine DLL_EXPORT
+#cmakedefine HAVE_UNISTD_H 
+#cmakedefine YY_NO_UNISTD_H 
