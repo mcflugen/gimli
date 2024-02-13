7f7048c95a5e3f2ee5fed045eb35d3566de28162
mcflugen <mcflugen@gmail.com>
2024-02-12 18:42:48 -0700

    rename requirements files

--- noxfile.py
+++ noxfile.py
@@ -50,8 +50,8 @@ def build_docs(session: nox.Session) -> None:
     build_generated_docs(session)
 
     session.install(
-        *("-r", "requirements-docs.txt"),
-        *("-r", "requirements.txt"),
+        *("-r", "requirements-docs.in"),
+        *("-r", "requirements.in"),
     )
     session.install(".")
 
@@ -138,7 +138,7 @@ def clean(session):
             shutil.rmtree(".pytest_cache", ignore_errors=True)
             shutil.rmtree(".venv", ignore_errors=True)
 
-            for pattern in ["*.py[co]", "__pycache__"]:
+            for pattern in ["*.py[co]", "__pycache__", "*.so"]:
                 _clean_rglob(pattern)
 
 
