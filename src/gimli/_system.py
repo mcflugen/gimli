4af69c1f521a0d03a94391f688c92551ae40dd3b
mcflugen <mcflugen@gmail.com>
2024-02-27 15:57:00 -0700

    pre-load unit system with some known units

--- src/gimli/_system.py
+++ src/gimli/_system.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
-from collections import UserDict
+import os
+from collections.abc import Generator
+from collections.abc import Mapping
+from xml.etree import ElementTree
 
 from gimli._udunits2 import Unit
 from gimli._udunits2 import _UnitSystem
 
 
-class UnitSystem(UserDict[str, Unit], _UnitSystem):
+class UnitSystem(Mapping[str, Unit], _UnitSystem):
 
     """A system of units.
 
@@ -31,7 +34,53 @@ class UnitSystem(UserDict[str, Unit], _UnitSystem):
     def __init__(self, filepath: str | None = None):
         self.data: dict[str, Unit] = {}
 
+        for symbol in _load_all_symbols_from_database(self.database):
+            self[symbol]
+
     def __getitem__(self, key: str) -> Unit:
+        key = key.strip()
+
         if key not in self.data:
-            self.data[key] = self.Unit(key)
-        return self.data[key]
+            unit = self.Unit(key)
+            if unit.name not in self.data:
+                self.data[unit.name] = unit
+            return self.data[unit.name]
+        else:
+            return self.data[key]
+
+    def __iter__(self) -> Generator[str, None, None]:
+        yield from self.data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return repr(self.data)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, UnitSystem):
+            return NotImplemented
+        return os.path.samefile(self.database, other.database)
+
+
+def _load_all_symbols_from_database(path: str) -> set[str]:
+    base = os.path.dirname(path)
+
+    roots = [
+        ElementTree.parse(os.path.join(base, child.text)).getroot()
+        for child in ElementTree.parse(path).getroot()
+        if child.tag == "import" and isinstance(child.text, str)
+    ]
+
+    symbols: set[str] = set()
+    for root in roots:
+        symbols |= {
+            element.text
+            for element in root.findall("unit/symbol")
+            if isinstance(element.text, str)
+        }
+
+    return symbols
