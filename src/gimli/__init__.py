bb7888ae77897891ef8d180bf2b027e8c35d0678
mcflugen <mcflugen@gmail.com>
2026-03-03 08:20:27 -0700

    rename convert to convert_units

--- src/gimli/__init__.py
+++ src/gimli/__init__.py
@@ -1,13 +1,13 @@
 from gimli._api import UnitConverter
-from gimli._api import convert
+from gimli._api import convert_units
 from gimli._api import get_unit_system
 from gimli._api import units
 from gimli._version import __version__
 
 __all__ = [
     "__version__",
-    "convert",
     "UnitConverter",
+    "convert_units",
     "get_unit_system",
     "units",
 ]
